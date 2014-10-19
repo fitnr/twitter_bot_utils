61e46725d1000a4806a7823c91fe1ec13ea7987d
fitnr <fitnr@fakeisthenewreal>
2014-10-19 01:57:53 -0400

    Add fave_mentions entry point

--- tweet_utils/fave_mentions.py
+++ tweet_utils/fave_mentions.py
@@ -1,13 +1,18 @@
 #!/usr/local/bin/python
-import tweet_utils
+from tweet_utils import utils
 import argparse
 
-parser = argparse.ArgumentParser(description='fave mentions')
-parser.add_argument('-u', '--screen_name', type=str, help='User who will be doing the favoriting')
-parser.add_argument('-a', '--app', type=str, help='Name of the app to authenticate with')
-args = parser.parse_args()
 
-screen_name = args.screen_name
-app = args.app
+def main():
+    parser = argparse.ArgumentParser(description='fave mentions')
+    parser.add_argument('-u', '--screen_name', type=str, help='User who will be doing the favoriting')
+    parser.add_argument('-a', '--app', type=str, help='Name of the app to authenticate with')
+    args = parser.parse_args()
 
-api = tweet_utils.utils.fave_mentions(app=app, screen_name=screen_name)
+    screen_name = args.screen_name
+    app = args.app
+
+    utils.fave_mentions(app=app, screen_name=screen_name)
+
+if __name__ == '__main__':
+    main()
