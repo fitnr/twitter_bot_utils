836b37d48b6867bfc39ae2aa996c263ecc152660
fitnr <fitnr@fakeisthenewreal>
2014-10-19 21:41:43 -0400

    use local file for config

--- tweet_utils/config.py
+++ tweet_utils/config.py
@@ -1,16 +1,52 @@
-from pkg_resources import resource_stream
+from os import path
 import yaml
 
-config_handle = resource_stream('tweet_utils', '../conf.yaml')
-config = yaml.load(config_handle)
+config = None
+
+def load_config(user_conf=None):
+    '''Load a user config file or one from a standard place'''
+    global config
+
+    if config is None:
+
+        file_name = ''
+
+        if user_conf and path.exists(user_conf):
+            file_name = user_conf
+
+        else:
+            local_conf = path.join(path.expanduser('~'), 'bots.yaml')
+
+            if not path.exists(local_conf):
+                local_conf = path.join(path.expanduser('~'), 'bots/bots.yaml')
+
+            if path.exists(local_conf):
+                file_name = local_conf
+
+        try:
+            with open(file_name, 'r') as f:
+                config_handle = f.read()
+
+            config = yaml.load(config_handle)
+
+        except (AttributeError, IOError), e:
+            if user_conf:
+                msg = 'Custom config file not found: {0}'.format(user_conf)
+
+            else:
+                msg = 'Config file not found in ~/bots.yaml or ~/bots/bots.yaml'
+
+            raise IOError(msg)
 
 def user(username):
+    load_config()
     user = config['users'].get(username, None)
     if user is None:
         raise IndexError
     return user
 
 def app(appname):
+    load_config()
     app = config['apps'].get(appname, None)
     if app is None:
         raise IndexError
--- tweet_utils/since.py
+++ tweet_utils/since.py
@@ -1,9 +1,23 @@
 from os import path
+import config
 import json
 
-SINCE_ID_FILE = path.join(path.dirname(__file__), '..', '..', 'data/since/since_ids.json')
+SINCE_ID_FILE = None
+
+
+def _set_path():
+    global SINCE_ID_FILE
+    if SINCE_ID_FILE is None:
+        config.load_config()
+
+        pth = config.config['since_id_file'].format(data_path=config.config['data_path'])
+
+        SINCE_ID_FILE = path.join(path.expanduser(pth))
+
 
 def save_since_id(user, _id):
+    _set_path()
+
     with open(SINCE_ID_FILE, 'r') as f:
         ids = json.load(f)
 
@@ -12,9 +26,10 @@ def save_since_id(user, _id):
     with open(SINCE_ID_FILE, 'w') as f:
         json.dump(ids, f)
 
+
 def since_id(user):
+    _set_path()
     with open(SINCE_ID_FILE, 'r') as f:
         ids = json.load(f)
 
     return ids[user]
-
