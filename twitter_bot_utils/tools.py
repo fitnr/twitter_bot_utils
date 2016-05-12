f268b9de44c2a3907096b1fb2520b7aaa9fd1832
fitnr <fitnr@fakeisthenewreal>
2016-05-12 11:52:40 -0400

    update © year

--- twitter_bot_utils/tools.py
+++ twitter_bot_utils/tools.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright 2014 Neil Freeman
+# Copyright 2014-16 Neil Freeman
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
